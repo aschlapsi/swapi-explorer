f0943c70c7b4e2c3a9b080ab44815f5865b31165
Andreas Schlapsi <andreas.schlapsi@gmail.com>
2020-11-01 21:00:50 +0100

    Added "load more" functionality

--- swexplorer/swcollections/views.py
+++ swexplorer/swcollections/views.py
@@ -15,10 +15,15 @@ def fetch_collection(request):
 
 
 def display_file_data(request, file_id):
+    if request.method == 'POST':
+        count = int(request.POST.get('next_count', 10))
+    else:
+        count = 10
     data_file = get_object_or_404(CollectionFile, pk=file_id)
     table_as_html = petl.MemorySource()
-    data_file.get_table().head(10).tohtml(table_as_html)
+    data_file.get_table().head(count).tohtml(table_as_html)
     return render(request, 'swcollections/file_data.html', {
+        'next_count': count + 10,
         'filename': data_file.file_name(),
         'filedata': table_as_html.getvalue().decode('utf-8'),
     })

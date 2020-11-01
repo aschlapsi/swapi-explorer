179b9744f58964b1e9b003bd61881eea992f6fd7
Andreas Schlapsi <andreas.schlapsi@gmail.com>
2020-11-01 16:57:54 +0100

    Added list of files

--- swexplorer/swcollections/urls.py
+++ swexplorer/swcollections/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('fetch', views.fetch_collection, name='fetch-collection'),
+    path('', views.FileList.as_view(), name='file-list'),
 ]
--- swexplorer/swcollections/views.py
+++ swexplorer/swcollections/views.py
@@ -1,5 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import redirect
+from django.views.generic import ListView
+from swcollections.models import CollectionFile, fetch_characters
 
 
-def index(request):
-    return render(request, 'swcollections/index.html')
+class FileList(ListView):
+    model = CollectionFile
+    context_object_name = 'collection_files'
+
+
+def fetch_collection(request):
+    fetch_characters()
+    return redirect('file-list')
